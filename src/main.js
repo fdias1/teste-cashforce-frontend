0b2be56bece8f2475156f86d9626db8f0aab5dad
Fábio Dias <fabbiodiaz@gmail.com>
2021-01-13 19:11:20 -0300

    configuração do vue router

--- src/main.js
+++ src/main.js
@@ -1,8 +1,25 @@
 import Vue from 'vue'
 import App from './App.vue'
+import vueRouter from 'vue-router'
 
+import NotasFiscais from './components/NotasFiscais'
+Vue.use(vueRouter)
 Vue.config.productionTip = false
 
+const router = new vueRouter({
+  routes:[
+    {
+      path:'/notas',
+      component:NotasFiscais
+    },
+    {
+      path:'/',
+      redirect:'/notas'
+    }
+  ]
+})
+
 new Vue({
+  router,
   render: h => h(App),
 }).$mount('#app')
