2278c625f119565176fb33618b011d778f4ca77c
Fábio Dias <fabbiodiaz@gmail.com>
2021-01-14 11:16:47 -0300

    adicionado link para a home na logo da empresa

--- src/components/Logo.vue
+++ src/components/Logo.vue
@@ -1,6 +1,8 @@
 <template> 
-    <div class="logo">
-        <img src="../assets/logo-nome.png" alt="CashForce">
+    <div class="logo side-shadow">
+        <Router-link to="/home">
+            <img src="../assets/logo-nome.png" alt="CashForce">
+        </Router-link>
     </div>
 </template>
 
@@ -18,9 +20,6 @@
         grid-area: logo;
         padding: 25px 24px;
         background-color: #FBFDFE;
-        -webkit-box-shadow: inset -30px 0px 30px -30px rgba(221,221,221,1);
-        -moz-box-shadow: inset -30px 0px 30px -30px rgba(221,221,221,1);
-        box-shadow: inset -30px 0px 30px -30px rgba(221,221,221,1);
     }
     img {
         width: 160px;
