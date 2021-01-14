6a79bf25c7cc76c3a9e08df3db1da73b623b702d
Fábio Dias <fabbiodiaz@gmail.com>
2021-01-14 14:07:00 -0300

    diretiva do componente infoCedente trocada para v-show, para contornar o atraso gerado na primeira renderização do componente

--- src/services/api.js
+++ src/services/api.js
@@ -1,9 +1,13 @@
 import axios from 'axios'
 
 const baseURL = process.env.VUE_APP_API_BASE_URL
+const token = process.env.VUE_APP_API_TOKEN
 
 const api = axios.create({
     baseURL,
+    headers:{
+        token
+    }
 })
 
 export default api
